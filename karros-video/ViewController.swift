d4cea2f3f736d63e581939427a481373ebb9f63e
Hoang Lu <hoanglu@vulcanlabs.co>
2020-04-16 13:44:57 +0700

    Initial Commit

---
+++ karros-video/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  karros-video
+//
+//  Created by Hoang Lu on 4/16/20.
+//  Copyright © 2020 Hoang Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
